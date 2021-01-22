a0728b12359599460abf650c9fb74fc0a28294a2
SAMEER TARIQ <imsamtar@gmail.com>
2021-01-22 13:14:07 +0500

    update

--- src/App.svelte
+++ src/App.svelte
@@ -4,20 +4,18 @@
 
 <main>
 	<h1>Hello {name}!</h1>
-	<p>
-		Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
-		how to build Svelte apps.
-	</p>
 </main>
 
 <style lang="scss">
+	$color: #ff3e00;
+
 	main {
 		text-align: center;
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
 		h1 {
-			color: #ff3e00;
+			color: $color;
 			text-transform: uppercase;
 			font-size: 4em;
 			font-weight: 100;
